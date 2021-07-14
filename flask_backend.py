0d2e91c246d3d0d254887c5c8ad751cdb5cb1c78
Rahul <rahulbhataniya2468@gmail.com>
2021-07-14 15:17:41 +0530

    final testing done

--- flask_backend.py
+++ flask_backend.py
@@ -6,6 +6,7 @@ app = Flask(__name__, static_folder='static')
 @app.route("/")
 def success():
     path='que_ans_video.mp4'
+    path='videoplayback.mp4'
     #time_subtitle=[]
     time_subtitle = swt.get_large_audio_transcription('static'+'/'+path)
     time_subtitle.append(path)
@@ -13,7 +14,5 @@ def success():
 		
 if __name__ == '__main__':
    app.run(debug = True)
-
-
 
 
